adf3a43ccbfadea473627c60d96eaebbaa825eed
Ba6aka <bam6ykba6aka@gmail.com>
2025-10-23 22:43:38 +0300

    feat: add user API endpoints and sample user data

--- index.js
+++ index.js
@@ -3,6 +3,23 @@ import express from "express";
 const app = express();
 const port = process.env.PORT || 1337
 
+const users = [
+  { 'id': 1, 'name': 'Ba6aka' },
+  { 'id': 2, 'name': 'Marina' }
+]
+
 app.listen(port, () => {
   console.log(`server started on port ${port}`)
+})
+
+app.get('/api/users', (request, response) => {
+  response.status(201).send(users)
+})
+
+app.get('/api/users/:id', (request, response) => {
+  const parsedId = +request.params.id
+
+  if (isNaN(parsedId)) return response.status(400).send('id is not a number')
+
+  response.send(users.find((user) => user.id == parsedId) || "ivalid user id")
 })
